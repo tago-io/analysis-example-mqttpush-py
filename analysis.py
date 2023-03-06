364be86e9fe2202c5228903eaf396106e9a7b8d5
Mateus Cardoso <mateus.informatica44@gmail.com>
2023-03-06 15:07:25 -0300

    Update script to new version of sdk-python.

--- analysis.py
+++ analysis.py
@@ -1,37 +1,60 @@
-# Analysis Example
-# Get Device List
-#
-# Snippet to push data to MQTT. Follow this pattern within your application
-# If you want more details about MQTT, search "MQTT" in TagoIO help center.
-# You can find plenty of documentation about this topic.
-# TagoIO Team.
-#
-# How to use?
-# In order to trigger this analysis you must setup a Dashboard.
-# Create a Widget "Form" and enter the variable 'push_payload' for the device you want to push with the MQTT.
-# In User Control, select this Analysis in the Analysis Option.
-# Save and use the form.
+"""
+Analysis Example
+Get Device List
 
-from tago import Analysis
-from tago import Services
+Snippet to push data to MQTT. Follow this pattern within your application
+If you want more details about MQTT, search "MQTT" in TagoIO help center.
+You can find plenty of documentation about this topic.
+TagoIO Team.
+
+How to use?
+In order to trigger this analysis you must setup a Dashboard.
+Create a Widget "Form" and enter the variable 'push_payload' for the device you want to push with the MQTT.
+In User Control, select this Analysis in the Analysis Option.
+Save and use the form.
+"""
+from tagoio_sdk import Analysis, Services
 
 
 # The function myAnalysis will run when you execute your analysis
-def myAnalysis(context,scope):
-  # my_data_bucket = scope[0]['bucket'] # for legacy
-  my_data_bucket = scope[0]['device'] # for immutable/mutable
-  my_data_value = scope[0]['value']
-  # Create your data object to push to MQTT
-  # In this case we're sending a JSON.
-  # You can send anything you want.
-  my_data_json = {'variable': 'temperature_celsius','value': my_data_value,'unit': 'C'}
-  # Create topic, retain and qos, you chooses
-  topic = 'test'
-  retain = False
-  qos = 1
-  # Publishing to MQTT
-  MQTT = Services(context.token).MQTT
-  result = MQTT.publish(my_data_json,my_data_bucket,topic,retain,qos)
-  context.log(result)
+def my_analysis(context, scope: list[dict]) -> None:
+    if not scope:
+        return print("This analysis must be triggered by a dashboard.")
+
+    my_data = [obj for obj in scope if obj["variable"] == "push_payload"]
+    if not my_data:
+        return print("Couldn't find any variable in the scope.")
+
+    # Create your data object to push to MQTT
+    # In this case we're sending a JSON object.
+    # You can send anything you want.
+    # Example:
+    # const myDataObject = 'This is a string';
+    my_data_object = {
+        "variable": "temperature_celsius",
+        "value": (int(my_data[0]["value"]) - 32) * (5 / 9),
+        "unit": "C",
+    }
+
+    # Create a object with the options you chooses
+    options = {
+        "retain": False,
+        "qos": 0,
+    }
+
+    # Publishing to MQTT
+    MQTT = Services({"token": context.token}).MQTT
+    result = MQTT.publish(
+        {
+            # bucket: myData.bucket, // for legacy devices
+            "bucket": my_data[0]["device"],  # for immutable/mutable devices
+            "message": str(my_data_object),
+            "topic": "tago/my_topic",
+            "options": options,
+        }
+    )
+    print(result)
+
+
 # The analysis token in only necessary to run the analysis outside TagoIO
-Analysis('my analysis token here').init(myAnalysis)
+Analysis({"token": "MY-ANALYSIS-TOKEN-HERE"}).init(my_analysis)
